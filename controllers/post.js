0b198dd3e77be52d9f9adf75b61286a7b0355a6b
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-07-24 14:21:08 -0500

    updat to post should be able to create posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Post    = require('../models/post.js');
+const User    = require('../models/user.js')
 
 
 
@@ -12,14 +13,35 @@ router.get('/', (req, res) => {
 
 
 
-router.post('/', async (req, res) => {
+router.get('/makepost', async (req, res) => {
 	try{
-		
+		const foundUser = await User.find({});
+		res.render('post/new.ejs', {
+			allUsers: foundUser
+		})
 
 	}catch(err){
 		res.send(err)
 	}
 })
+
+router.post('/', async (req, res) => {
+	try{
+		const user = await User.find({'username': req.session.username});
+		const post = {
+			userId: user.id,
+			photoURL: req.body.photoURL,
+			status: req.body.status
+		}
+		const newPost = await Post.create(post)
+		user.post.push(newPost);
+		user.save();
+		res.redirect('news_feed.ejs')
+	}catch(err){
+		res.send(err)
+	}
+})
+
 
 
 module.exports = router;
--- models/post.js
+++ models/post.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema({
 	userId: String,
 	caption: String,
-	photo: String,
+	photoURL: String,
 	status: String,
-	date: Date
+	date: {
+		type: Date,
+		default: Date.now
+	}
 })
 module.exports = mongoose.model('Post', postSchema)
