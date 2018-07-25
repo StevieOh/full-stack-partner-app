b8afcba1b8c945d1522af3b7b3ba29f51f2cc5ec
Your Name <genejune4@hotmail.com>
2018-07-25 11:23:30 -0500

    user show route is done

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const User = require('../models/user')
 
 
+router.use((req, res, next) => {
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log('checking login');
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+
+  if(req.session.loggedIn === false){
+    res.redirect('/')
+  }
+  next();
+})
+
+
 router.get('/index.ejs', (req, res) => {
   res.send('newsfeed') 
 })
@@ -27,12 +45,41 @@ router.get('/index', async (req, res) => {
 //////////////////////////
 router.get('/userprofile', async (req, res) => {
   try{
-    const user = await User.find({'username': req.session.username})
-    
+    const foundUser = await User.find({});
+    const mainUser = await User.findOne({'username': req.session.username})
+    console.log(mainUser, " this is mainUser in the user show route ")
+    console.log(req.session.username, " this is req.session.username in user show route")
+    console.log(req.session.loggedIn, " this is req.session.loggedIn in user show route")
+    res.render('users/show.ejs', {
+      user: mainUser,
+      post: req.session.post
+    })
   }catch(err){
     res.send(err)
   }
 })
+
+//////////////////////////
+// edit user
+//////////////////////////
+
+router.post('/editprofile', async (req, res) => {
+  try{
+    const user = await User.find({'username': req.session.username})
+  }catch(err){
+    res.send(err)
+  }
+})
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router
