21121c0bb8c33356f71b88a13c744caa3226d29c
Your Name <genejune4@hotmail.com>
2018-08-28 13:13:04 -0500

    made an example group messaging area

--- capstone/example/index.js
+++ capstone/example/index.js
@@ -1,5 +1,35 @@
-const app = require('express');
-const http - require('http').server(app)
+const app = require('express')();
+const http = require('http').Server(app)
+const io = require('socket.io')(http);
 
 
-app.get('/', function)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+  })
+});
+
+io.on('connection', (socket) => {
+socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  });
+});
+
+http.listen(3002, () => {
+  console.log("I'm listening on 3002");
+});
